f2f67052a8b32ce39d595dad709cbf7ca310b911
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-21 22:12:48 +0900

    name部分をlastとfirstで分け、full_nameメソッドも追加

--- environment/sample_app/example_user.rb
+++ environment/sample_app/example_user.rb
@@ -1,12 +1,17 @@
 class User
-  attr_accessor :name, :email
+  attr_accessor :first_name, :last_name, :email
 
   def initialize(attributes = {})
-    @name  = attributes[:name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @email = attributes[:email]
   end
 
+  def full_name
+    "#{@last_name} #{@first_name}"
+  end
+
   def formatted_email
-    "#{@name} <#{@email}>"
+    "#{@first_name}_#{@last_name} <#{@email}>"
   end
 end
